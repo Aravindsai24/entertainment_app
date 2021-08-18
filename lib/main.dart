0ea954aa024911f5813a45258e07e58629e217c1
aravindsai24 <tas10@iitbbs.ac.in>
2021-08-18 17:58:27 +0530

    initial setup - bottom navigation bar

---
+++ lib/main.dart
@@ -0,0 +1,86 @@
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({Key? key}) : super(key: key);
+
+  static const String _title = 'Entertainment App';
+
+  @override
+  Widget build(BuildContext context) {
+    return const MaterialApp(
+      title: _title,
+      home: MyStatefulWidget(),
+    );
+  }
+}
+
+class MyStatefulWidget extends StatefulWidget {
+  const MyStatefulWidget({Key? key}) : super(key: key);
+
+  @override
+  _MyStatefulWidgetState createState() => _MyStatefulWidgetState();
+}
+
+class _MyStatefulWidgetState extends State<MyStatefulWidget> {
+  int _selectedIndex = 0;
+  static const optionStyle =
+      TextStyle(fontSize: 30, fontWeight: FontWeight.bold) ;
+  static const List<Widget> _widgetOptions = <Widget>[
+    Text(
+      'Index 0: Movies',
+      style: optionStyle,
+    ),
+    Text(
+      'Index 1: Sports',
+      style: optionStyle,
+    ),
+    Text(
+      'Index 2: News',
+      style: optionStyle,
+    ),
+  ];
+
+  void _onItemTapped(int index) {
+    setState(() {
+      _selectedIndex = index;
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('Entertainment App'),
+      ),
+      body: Center(
+        child: _widgetOptions.elementAt(_selectedIndex),
+      ),
+      bottomNavigationBar: BottomNavigationBar(
+        items: const <BottomNavigationBarItem>[
+          BottomNavigationBarItem(
+            icon: Icon(Icons.movie),
+            label: 'Movies',
+          ),
+          BottomNavigationBarItem(
+            icon: Icon(Icons.sports_football),
+            label: 'Sports',
+          ),
+          BottomNavigationBarItem(
+            icon: Icon(CupertinoIcons.news_solid),
+            label: 'News',
+          ),
+        ],
+        currentIndex: _selectedIndex,
+        selectedItemColor: Colors.amber[800],
+        onTap: _onItemTapped,
+      ),
+    );
+  }
+}
+
+
